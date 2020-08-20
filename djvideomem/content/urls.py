c1d6cf23e3be0b236821d79f0581b2d7a2696883
mattfreire <freire1996@gmail.com>
2020-08-20 17:54:16 +0200

    Course details

--- djvideomem/content/urls.py
+++ djvideomem/content/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CourseListView
+from .views import CourseListView, CourseDetailView
 
 app_name = "content"
 
 urlpatterns = [
-    path("", CourseListView.as_view(), name='course-list')
+    path("", CourseListView.as_view(), name='course-list'),
+    path("<slug>/", CourseDetailView.as_view(), name='course-detail')
 ]
--- djvideomem/content/views.py
+++ djvideomem/content/views.py
@@ -4,3 +4,8 @@ from .models import Course
 class CourseListView(generic.ListView):
     template_name = "content/course_list.html"
     queryset = Course.objects.all()
+
+
+class CourseDetailView(generic.DetailView):
+    template_name = "content/course_detail.html"
+    queryset = Course.objects.all()
