18c78067aa69eb0d12e16b5cf75a6708da21baac
mattfreire <freire1996@gmail.com>
2020-08-25 16:44:53 +0200

    Subscription and Pricing

--- djvideomem/content/admin.py
+++ djvideomem/content/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Course, Video
+from .models import Course, Video, Pricing, Subscription
 
 admin.site.register(Course)
 admin.site.register(Video)
+admin.site.register(Pricing)
+admin.site.register(Subscription)
--- djvideomem/content/models.py
+++ djvideomem/content/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+from django.conf import settings
 from django.db.models.signals import pre_save, post_save
 from django.utils.text import slugify
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Pricing(models.Model):
+    name = models.CharField(max_length=100)  # Basic / Pro / Premium
+
+    def __str__(self):
+        return self.name
+
+
+class Subscription(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pricing = models.ForeignKey(Pricing, on_delete=models.CASCADE, related_name='subscriptions')
+    created = models.DateTimeField(auto_now_add=True)
+    stripe_subscription_id = models.CharField(max_length=50)
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.email
 
 
 class Course(models.Model):
@@ -48,5 +69,11 @@ def pre_save_video(sender, instance, *args, **kwargs):
         instance.slug = slugify(instance.title)
 
 
+def post_save_user(sender, instance, created, *args, **kwargs):
+    if created:
+        free_trial_pricing = Pricing.objects.get(name='Free Trial')
+        Subscription.objects.create(user=instance, pricing=free_trial_pricing)
+
+post_save.connect(post_save_user, sender=User)
 pre_save.connect(pre_save_course, sender=Course)
 pre_save.connect(pre_save_video, sender=Video)
